235905ba7c0905b9e0968dc315ba254487cb7309
Manik <tonysins43@gmail.com>
2023-07-24 13:09:22 +0600

    some changes

--- src/Pages/MyCollage/MyCollage.jsx
+++ src/Pages/MyCollage/MyCollage.jsx
@@ -19,31 +19,36 @@ const MyCollage = () => {
     setIsOpen(false);
   };
 
-  // handle rating 
-  const handleRating = (event)=>{
+  // handle rating
+  const handleRating = (event) => {
     event.preventDefault();
     const form = event.target;
     const reviewDescription = form.reviewDescription.value;
     const rating = form.rating.value;
-    const review = {reviewDescription:reviewDescription,rating:rating}
-    console.log(review)
-    fetch(`https://collage-nest-server.vercel.app/review/${collage?.collageId}`,{
-      method:"PUT",
-      headers:{"content-type":"application/json"},
-      body:JSON.stringify(review)
-    })
-    .then(res=>res.json())
-    .then(data=>{
-      if(data.modifiedCount > 0){
-        Swal.fire("Rating Added Successfully", "success");
-        setIsOpen(false)
+    const review = { reviewDescription: reviewDescription, rating: rating };
+    console.log(review);
+    fetch(
+      `https://collage-nest-server.vercel.app/review/${collage?.collageId}`,
+      {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(review),
       }
-    })
-  }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          Swal.fire("Rating Added Successfully", "success");
+          setIsOpen(false);
+        }
+      });
+  };
 
   useEffect(() => {
     setLoading(true);
-    fetch(`https://collage-nest-server.vercel.app/myCollage?email=${user?.email}`)
+    fetch(
+      `https://collage-nest-server.vercel.app/myCollage?email=${user?.email}`
+    )
       .then((res) => res.json())
       .then((data) => {
         setMyCollages(data);
@@ -56,48 +61,52 @@ const MyCollage = () => {
   }
   return (
     <div className="mt-32 md:16 mb-12">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {myCollages.map((collage) => (
-          <div
-            key={collage?._id}
-            className="card card-compact w-full bg-base-100 shadow-xl"
-          >
-            <figure>
-              <img
-                className="w-full h-56"
-                src={collage?.collageImage}
-                alt="Shoes"
-              />
-          </figure>
-            <div className="card-body">
-              <h2 className="card-title">{collage?.collageName}</h2>
-              <p>Number Of Research: {collage?.num_of_research}</p>
-              <div className="flex justify-between">
-                <div>
-                  <h4 className="text-xl font-bold">Events</h4>
-                  {collage.events.map((event, i) => (
-                    <li key={i}>{event}</li>
-                  ))}
-                </div>
-                <div className="">
+      {myCollages.length < 1 ? (
+        <h1 className="text-center text-4xl font-bold">You Did Not Apply Any Collage</h1>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {myCollages.map((collage) => (
+            <div
+              key={collage?._id}
+              className="card card-compact w-full bg-base-100 shadow-xl"
+            >
+              <figure>
+                <img
+                  className="w-full h-56"
+                  src={collage?.collageImage}
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{collage?.collageName}</h2>
+                <p>Number Of Research: {collage?.num_of_research}</p>
+                <div className="flex justify-between">
+                  <div>
+                    <h4 className="text-xl font-bold">Events</h4>
+                    {collage.events.map((event, i) => (
+                      <li key={i}>{event}</li>
+                    ))}
+                  </div>
+                  <div className="">
                     <h4 className="text-xl font-bold">Sports</h4>
-                    {
-                        collage.sports.map((sport,i)=><li key={i}>{sport}</li>)
-                    }
+                    {collage.sports.map((sport, i) => (
+                      <li key={i}>{sport}</li>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <div className="card-actions justify-end">
-                <button
-                  onClick={() => openModal(collage)}
-                  className="primary-btn"
-                >
-                  Add Review
-                </button>
+                <div className="card-actions justify-end">
+                  <button
+                    onClick={() => openModal(collage)}
+                    className="primary-btn"
+                  >
+                    Add Review
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
       {isOpen && (
         <div className="fixed inset-0  flex items-center justify-center z-30 shadow-xl">
           <div className="absolute px-8 md:px-16 bg-[#F3F3F3] lg:w-2/5 p-6 rounded-lg">
